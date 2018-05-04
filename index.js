d224c8a8a8d32889e13c32ee22b39454f03dabaa
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-04 20:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,18 @@ class Route {
     let blocks = 0
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
-    // let i = for (const element of eastWest) {
-    //   element === this.beginningLocation.horizontal
-    // }
-    // let j = for (const element of eastWest) {
-    //   element === this.endingLocation.horizontal
-    // }
+    for (const element of eastWest) {
+      if (element === this.beginningLocation.horizontal) {
+        let i = element
+      }
+    }
+    for (const element of eastWest) {
+      if (element === this.endingLocation.horizontal) {
+        let j = element
+      }
+    }
 
-    // blocks += Math.abs(i - j)
+    blocks += Math.abs(i - j)
 
     blocks += Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
 
